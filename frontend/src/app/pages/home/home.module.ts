dd41ffe886657f239b0aa72add11b233c446220f
janiscostadelli <jcostadelli@gmail.com>
2022-04-16 13:08:30 -0300

    add configs

--- frontend/src/app/pages/home/home.module.ts
+++ frontend/src/app/pages/home/home.module.ts
@@ -5,16 +5,18 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HeaderModule } from 'src/app/shared/header/header.module';
 import { TaskItemModule } from 'src/app/shared/task-item/task-item.module';
 import { AddTaskModalModule } from 'src/app/shared/add-task-modal/add-task-modal.module';
+import { HomeComponent } from './home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
     HeaderModule,
     TaskItemModule,
     AddTaskModalModule
-  ]
+  ],
+  exports: [HomeComponent]
 })
 export class HomeModule { }
