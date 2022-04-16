a6b03998af50bbf9b53e3dfcb9aeef20996e374c
janiscostadelli <jcostadelli@gmail.com>
2022-04-16 12:24:40 -0300

    add pages and components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskModalComponent } from './shared/add-task-modal/add-task-modal.component';
+import { TaskItemComponent } from './shared/task-item/task-item.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LoginModule } from './pages/login/login.module';
+import { HomeComponent } from './pages/home/home.component';
+import { HomeModule } from './pages/home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskModalComponent,
+    TaskItemComponent,
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
