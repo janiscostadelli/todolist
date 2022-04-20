f5820ad7534725a087f686b75580e212243d91b9
Nicolas <nicolasalexandre0001@gmail.com>
2022-04-20 15:15:25 -0300

    heroku config

---
+++ frontend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const appName = 'frontend';
+
+const outputPath = `${__dirname}/dist/${appName}`;
+
+app.use(express.static(outputPath));
+
+app.get('*/', (req, res) => { 
+  res.sendFile(`${outputPath}/index.html`);
+});
+
+app.listen(process.ENV.PORT, () => {
+  console.log(`Listening on port ${process.ENV.PORT}`);
+});
