6d5f3098a594c594d69c02b1bd23c35eb995aa79
Nicolas <nicolasalexandre0001@gmail.com>
2022-04-21 13:30:22 -0300

    fix ngOnInit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,4 +6,6 @@ import { Component, OnInit } from '@angular/core'
 })
 export class HomeComponent implements OnInit {
   constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,4 +6,6 @@ import { Component, OnInit } from '@angular/core'
 })
 export class LoginComponent implements OnInit {
   constructor() {}
+
+  ngOnInit(): void {}
 }
