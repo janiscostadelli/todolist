fbefb114cee64536badcb4125883e6daf94cf098
Nicolas <nicolasalexandre0001@gmail.com>
2022-04-21 13:51:42 -0300

    fix port

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('*/', (req, res) => {
   res.sendFile(`${outputPath}/index.html`)
 })
 
-app.listen(3000, () => {
-  console.log('Express server listening on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env)
 })
