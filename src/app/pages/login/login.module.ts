83b247cf31843122037fe9169be5d4d555d27c60
janiscostadelli <jcostadelli@gmail.com>
2022-04-22 17:29:27 -0300

    add login page

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -5,6 +5,12 @@ import { LoginRoutingModule } from './login-routing.module';
 import { HeaderModule } from 'src/app/shared/header/header.module';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 
 @NgModule({
@@ -14,6 +20,10 @@ import { FormsModule } from '@angular/forms';
     LoginRoutingModule,
     HeaderModule,
     FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [LoginComponent]
 })
